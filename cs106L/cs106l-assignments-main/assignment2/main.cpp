edb39985f7aaac2f11d7d3899fc2cb3c8a75e411
dextermayhewjd <dexterding99@icloud.com>
2026-01-26 18:14:27 +0800

    as2 completed

--- cs106L/cs106l-assignments-main/assignment2/main.cpp
+++ cs106L/cs106l-assignments-main/assignment2/main.cpp
@@ -1,20 +1,12 @@
-/*
- * CS106L Assignment 2: Marriage Pact
- * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob Roberts-Baca.
- *
- * Welcome to Assignment 2 of CS106L! Please complete each STUDENT TODO
- * in this file. You do not need to modify any other files.
- *
- */
-
 #include <fstream>
 #include <iostream>
 #include <queue>
 #include <set>
+#include <stdexcept>
 #include <string>
 #include <unordered_set>
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+std::string kYourName = "Dexter Ding"; // Don't forget to change this!
 
 /**
  * Takes in a file name and returns a set containing all of the applicant names as a set.
@@ -29,7 +21,24 @@ std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
  */
 std::set<std::string> get_applicants(std::string filename) {
   // STUDENT TODO: Implement this function.
+  std::set<std::string> applicants;
+  
+  std::ifstream ifs(filename);
+   if(!ifs)
+   {
+      throw std::runtime_error("open failed");
+   }
+   
+   std::string line;
+   while(std::getline(ifs,line))
+   {
+      applicants.insert(line);
+   }
+
+   return applicants;
 }
+
+
 
 /**
  * Takes in a set of student names by reference and returns a queue of names
@@ -41,7 +50,28 @@ std::set<std::string> get_applicants(std::string filename) {
  */
 std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
   // STUDENT TODO: Implement this function.
+  std::queue<const std::string*> matching_name_pointers;
+  
+  auto name_first_space = name.find(' ');
+  auto name_first_name_initial = *(name.begin());
+  auto name_last_name_initial = *(name.begin()+name_first_space+1);
+
+  for(const auto& student:students)
+  {
+      auto first_space = student.find(' ');
+      auto first_name_initial = *(student.begin());
+      auto last_name_initial = *(student.begin()+first_space+1);
+      if(first_name_initial == name_first_name_initial && last_name_initial==name_last_name_initial)
+      {
+          matching_name_pointers.push(&student);
+      }
+  }
+  return matching_name_pointers;
 }
+
+
+
+
 
 /**
  * Takes in a queue of pointers to possible matches and determines the one true match!
@@ -53,8 +83,28 @@ std::queue<const std::string*> find_matches(std::string name, std::set<std::stri
  * @return        Your magical one true love.
  *                Will return "NO MATCHES FOUND." if `matches` is empty.
  */
-std::string get_match(std::queue<const std::string*>& matches) {
+std::string get_match(std::queue<const std::string*>& matches) 
+{
   // STUDENT TODO: Implement this function.
+  if (matches.empty()) 
+  {
+    return "NO MATCHES FOUND.";
+  }
+
+  const std::string* best = matches.front();
+  matches.pop();
+
+  while (!matches.empty()) 
+  {
+    const std::string* current = matches.front();
+    matches.pop();
+
+    if (*current < *best) 
+    {   // 字典序比较
+      best = current;
+    }
+  }
+  return *best;
 }
 
 /* #### Please don't remove this line! #### */
